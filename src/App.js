545c822d8e1685c5622795dec8036d8b7e48acef
andsimon3 <andsimon3@yandex.ru>
2020-10-12 13:02:03 +0300

    Host

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Host from './HostMenu.js';
 
 function App() {
+	function RenderHost(){
+		ReactDOM.render(
+		    <Host />,
+		  document.getElementById('root')
+		);
+	}
   return (
     <div className="App">
-    	<input type='button' value='HOST' onClick=''/>
+    	<input type='button' value='HOST' onClick={}/>
     	<input type='button' value='Controller' onClick=''/>
     </div>
   );
---
+++ src/HostMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+//import './App.css';
+
+function HostMenu() {
+  return (
+    <div className="App">
+    	HOST
+    </div>
+  );
+}
+
+export default HostMenu;
