422d7bcd5bd2bbaacda80d7eb1a9ce90b2c48eac
andsimon3 <andsimon3@yandex.ru>
2020-10-12 14:04:11 +0300

    Revert "Update HostMenu.js"
    This reverts commit 141c03813bd7c7227b4ad29efffbd413c170812e.

--- src/HostMenu.js
+++ src/HostMenu.js
@@ -3,19 +3,9 @@ import logo from './logo.svg';
 //import './App.css';
 
 function HostMenu() {
-	function WebRTC(){
-		const iceConfiguration = {
-    iceServers: [{urls: ''}]
-		}
-		const peerConnection = new RTCPeerConnection(iceConfiguration);
-		const dataChannel = peerConnection.createDataChannel();
-		const remoteDesc = new RTCSessionDescription(message.answer);
-		return remoteDesc;
-	}
   return (
     <div className="App">
     	HOST1
-    	<QRCode value={WebRTC()} />
     </div>
   );
 }
