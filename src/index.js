b1a4686d0ce95e9dde4d7a710b19726e5fdb7985
andsimon3 <andsimon3@yandex.ru>
2020-10-13 21:24:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-console.log(window.location)
+console.log(window.location.search);
 ReactDOM.render(
   <React.StrictMode>
     <App />
