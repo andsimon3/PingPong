4220532e91f8fab893c9075e241ed608899dc974
andsimon3 <andsimon3@yandex.ru>
2020-10-13 20:29:42 +0300

    Update HostMenu.js

--- src/HostMenu.js
+++ src/HostMenu.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import QRCode from'qrcode.react';
+import ReactDOM from 'react-dom';
 //import './App.css';
 
 function HostMenu() {
 	function ControllerConnect(){
-		const peerConnection = new RTCPeerConnection(configuration);
+		const peerConnection = new RTCPeerConnection();
+		const dataChannel = peerConnection.createDataChannel();
     	const offer = peerConnection.createOffer();
     	peerConnection.setLocalDescription(offer);
-
+    	ReactDOM.render(<QRCode value={offer}/>, document.getElementById('HostMenu'))
+    	peerConnection.addEventListener('datachannel', event => {
+    		const dataChannel = event.channel;
+		});
 	}
   return (
-    <div className="App">
-    	<QRCode value={offer}/>
+    <div id="HostMenu">
+    	
     </div>
   );
 }
