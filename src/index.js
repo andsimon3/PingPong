be5b339f7f826ec2e0c097eaa3ea0d5416fbc7b1
andsimon3 <andsimon3@yandex.ru>
2020-10-13 21:40:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Host from './HostMenu.js';
+import Controller from './Controller.js';
 import * as serviceWorker from './serviceWorker';
 
-console.log(window.location.search);
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const params = window.location.search;
+const nparams = params.substr(1,3);
+const vparams = params.substr(5);
+if(nparams=='sdp'){
+	ReactDOM.render(
+	  <React.StrictMode>
+	    <Controller />
+	  </React.StrictMode>,
+	  document.getElementById('root')
+	);
+}else{
+	ReactDOM.render(
+	  <React.StrictMode>
+	    <HostMenu />
+	  </React.StrictMode>,
+	  document.getElementById('root')
+	);
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
