9f5c2301d83e4b7df16a898845934ded65b9341e
andsimon3 <andsimon3@yandex.ru>
2020-10-13 14:27:49 +0300

    Update ControllerMenu.js

--- src/ControllerMenu.js
+++ src/ControllerMenu.js
@@ -18,9 +18,9 @@ function ControllerMenu() {
     	window.addEventListener("deviceorientation", handleOrientation1, true);
 	} else if (window.DeviceMotionEvent) {
     	window.addEventListener('devicemotion', handleOrientation2, true);
-} else {
-    window.addEventListener("MozOrientation", handleOrientation3, true);
-}
+	} else {
+    	window.addEventListener("MozOrientation", handleOrientation3, true);
+	}
   return (
     <div id="ControllerMenu">
     	Controller
