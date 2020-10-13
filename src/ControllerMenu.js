4c0eb1b0e22136258acddd04e8ffc55b8a95b58f
andsimon3 <andsimon3@yandex.ru>
2020-10-13 14:15:34 +0300

    Update ControllerMenu.js

--- src/ControllerMenu.js
+++ src/ControllerMenu.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 function ControllerMenu() {
 	window.addEventListener("deviceorientation", handleOrientation, true);
 	function handleOrientation(event){
-		ReactDOM.render(<div>event.alpha</div>, document.getElementById('ControllerMenu'))
+		ReactDOM.render(<div>{event.alpha}</div>, document.getElementById('ControllerMenu'))
 	}
   return (
     <div id="ControllerMenu">
