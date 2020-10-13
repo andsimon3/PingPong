fc9e2278141eab7f62b9f3d8588135f8e9c9ae76
andsimon3 <andsimon3@yandex.ru>
2020-10-13 14:09:16 +0300

    C

--- src/ControllerMenu.js
+++ src/ControllerMenu.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function ControllerMenu() {
+	window.addEventListener("deviceorientation", handleOrientation, true);
+	function handleOrientation(event){
+		console.log(event);
+	}
   return (
     <div className="App">
     	Controller
--- src/HostMenu.js
+++ src/HostMenu.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 //import './App.css';
 
 function HostMenu() {
+	function ControllerConnect(){
+	}
   return (
     <div className="App">
     	HOST1111
